201a58812d2cb2392423cdd8d94f85d12685bed9
Jens Albert <jens.n.albert@gmail.com>
2014-07-17 15:19:36 +0200

    Test eines Branches

--- hhush/hhush.c
+++ hhush/hhush.c
@@ -9,7 +9,7 @@ was aber zu problemen mit gcc führen kann; Der Befehl ignoriert das Funktionen "
 #include <stdarg.h>		//für variable Parameteranzahl
 
 /*******************************************
- *******Hier beginnt das History-Modul******
+*******Hier beginnt das History-Modul******
 ********************************************/
 
 struct Node
@@ -62,15 +62,24 @@ void NeuesElement(char Input[256])
 
 void ListeLöschen(int n)		// Wir müssen nie einzelne Knoten löschen, also reicht eine Funktion, die alle Knoten löscht, und somit ihren Speicherplatz wieder freizugeben
 {
+	FILE *HistoryFile = fopen(".hhush.histfile", "ab");	//Öffnet (und erstellt gegebenenfalls) die History-Datei
 	struct Node *Hilfszeiger = Anfang;
 	while (Anfang != NULL)
 	{
-		//Routine zur Dateispeicherung einbauen!; falls n=0 => nur löschen für "history -c", ansonten history speichern mit n=1
+		if (n == 1)
+		{
+			fputs(Anfang->Eingabe, HistoryFile);		//Routine zur Dateispeicherung ; falls n=0 => nur löschen für "history -c", ansonten history speichern mit n=1
+		}
 		Hilfszeiger = Anfang->Next;
 		free(Anfang);
 		Anfang = Hilfszeiger;
 	}
 	ListenAnzahl = 0;
+	if (ferror(HistoryFile))
+	{
+		//Fehlerbehandlung einbauen für Fehler beim Schreiben
+	}
+	fclose(HistoryFile);
 	return;
 }
 
@@ -101,21 +110,21 @@ void History(int parameterzahl, ...)
 	{
 		int k;
 		Hilfszeiger = Anfang;
-		for (k = 1; k  < n; k++)
+		for (k = 1; k < n; k++)
 		{
 			Hilfszeiger = Hilfszeiger->Next;
 		}
 		printf("%i %s", Zähler, Hilfszeiger);
 		Zähler++;
 	}
-	
+
 	return;
 }
 
 int main(void)
 {
 
-	{ 
+	{
 		char input[256];
 		NeuesElement(fgets(input, sizeof(input), stdin));
 		NeuesElement(fgets(input, sizeof(input), stdin));
@@ -123,7 +132,8 @@ int main(void)
 		NeuesElement(fgets(input, sizeof(input), stdin));
 		NeuesElement(fgets(input, sizeof(input), stdin));
 		NeuesElement(fgets(input, sizeof(input), stdin));
-		History(0); 
+		History(0);
+		ListeLöschen(1);
 	}
 
 
