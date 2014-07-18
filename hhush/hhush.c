c69bf720b843a5eb9dec077cbe1e2c5a93537e1f
Jens Albert <jens.n.albert@gmail.com>
2014-07-18 18:23:50 +0200

    Schritt 1 der Input-Behandlung implementiert

--- hhush/hhush.c
+++ hhush/hhush.c
@@ -43,18 +43,18 @@ void NeuesElement(char Input[256])
 	{
 		Anfang = NeuerNode;
 		NeuerNode->Next = NULL;
-		ListenAnzahl++;
 	}
 	else
 	{
 		Anfang->Prev = NeuerNode;
 		NeuerNode->Next = Anfang;
 		Anfang = NeuerNode;
-		ListenAnzahl++;
 
 		/* Wir fügen einen neuen Knoten immer am Anfang der Liste ein, dies tun wir, da wir so eine möglichst einfache Struktur haben
 		und desweiteren nie einen Knoten woanders einfügen müssen (da wir diese doppelt verkettete Liste nur für history [n] brauchen) */
 	}
+
+	ListenAnzahl++;
 
 	//strncpy(NeuerNode->Eingabe, Input, 256);
 
@@ -153,6 +153,135 @@ void History(int Sichern, int parameterzahl, ...)		//History(0,0) = history; His
 	return;
 }
 
+/*************************************************
+*******Hier beginnt das Stufe-1-Input-Modul ******
+*************************************************/
+
+int InputStufe_1Fehler;		//Wird verwendet um bei Fehlern in Stufe 1 weitere Stufen abzubrechen
+
+struct StackElement
+{
+	char InputText[256];
+	int Länge;
+	struct StackElement *Next;
+};
+
+struct StackElement *AnfangStack = NULL;
+int StackTiefe;
+
+void Push(char Input[256], int Länge)
+{
+	struct StackElement *NeuerNode = malloc(sizeof(struct StackElement));		//Speicher reservieren für das neue Stack Element
+
+	if (NeuerNode == NULL)	//Falls kein Speicher freigegeben werden konnte
+	{
+		FehlerFunktion("Es konnte kein Speicher für das StackElement reserviert werden.");
+		return;
+	}
+
+	if (AnfangStack == NULL)		//Falls die Liste leer isi
+	{
+		AnfangStack = NeuerNode;
+		NeuerNode->Next = NULL;
+	}
+	else
+	{
+		NeuerNode->Next = AnfangStack;
+		AnfangStack = NeuerNode;
+	}
+
+	StackTiefe++;
+
+	memcpy(NeuerNode->InputText, Input, 256 * sizeof(char)); //Sicherer als strncpy, da Bufferoverflows verhindert werden
+	NeuerNode->Länge = Länge;
+
+	return;
+}
+
+struct StackElement Pop(void)
+{
+	struct  StackElement Rückgabe;		//Wir kopieren das oberste Stackelement in Rückgabe
+
+	if (AnfangStack == NULL)	//Falls der Stack leer ist
+	{
+		if (StackTiefe != 0)
+		{
+			FehlerFunktion("Fehler in der Stacktiefe");
+		}
+		Rückgabe.Länge = -1;
+		Rückgabe.Next = NULL;
+	}
+	else
+	{
+		memcpy(Rückgabe.InputText, AnfangStack->InputText, 256 * sizeof(char));
+		Rückgabe.Länge = AnfangStack->Länge;
+		Rückgabe.Next = NULL;
+
+		struct StackElement *HilfsZeiger = AnfangStack->Next;	//Wir geben den Speicherplatz für das oberste Stackelement frei
+		free(AnfangStack);
+		AnfangStack = HilfsZeiger;
+
+		StackTiefe--;
+	}
+	
+	return Rückgabe;
+}
+
+void InputSchritt_1(char Input[256])
+{
+	InputStufe_1Fehler = 0;
+	int InputLänge;		//Speichert wie lang die Nutzereingabe ist, indem es angiebt Input[InputLänge]='\n'
+
+	for (InputLänge = 0; Input[InputLänge] != '\n'; InputLänge++)
+	{
+		if (InputLänge > 256)		//Schaut ob die Eingabe zu lang ist
+		{
+			FehlerFunktion("Eingabe zu lang");
+			InputStufe_1Fehler = 1;
+			return;
+		}
+	}
+
+	int Zähler;		//Zählt an welcher Stelle im String ein Pipe Symbol ist, von hinten an
+	for (Zähler= InputLänge; Input[Zähler] != '|'; Zähler--)
+	{
+		if (Zähler < 0)
+		{
+			break;
+		}
+	}
+
+	if (Zähler == 0)
+	{
+		//Fehlerbehandlung falls Eingabe mit '|' beginnt
+	}
+
+	if (Zähler < 0)		//Wenn kein (weiteres) Pipe Symbol gefunden worden ist
+	{
+		Push(Input, InputLänge);
+		return;
+	}
+	else
+	{
+		char EinzelInput[256];			// Hier wird das Kommando hinter dem letzen Pipe Symbol ('|') gespeichert
+		int RestInputZähler = 0;		//Für die Numerierung des RestInputs
+		int HilfsZähler;
+
+		for (HilfsZähler = Zähler + 1; HilfsZähler <= InputLänge; HilfsZähler++)
+		{
+			EinzelInput[RestInputZähler] = Input[HilfsZähler];
+			RestInputZähler++;
+		}
+		EinzelInput[RestInputZähler] = '\0';
+		Input[Zähler] = '\n';		//Das ehemals letze Pipes Symbol wird zu einer Newline, somit kürzen wir den String um den letzen Befehl
+		Input[Zähler+  1] = '\0';	//Den String beende wir noch mit \0
+		Push(EinzelInput, InputLänge-Zähler);
+		InputSchritt_1(Input);
+		return;
+	}
+}
+
+
 int main(void)
 {
 	/*******************************************
@@ -173,21 +302,54 @@ int main(void)
 	}
 
 	/*******************************************
-	*******Hier beginnt ******
+	*******Hier beginnt das Input-Modul ******
 	********************************************/
+		char Input[256];	//Wird zur Speicherung der Nutzereingabe verwendet
+
+
+
+
+		while (1)
+		{
+			fgets(Input, sizeof(Input), stdin);
+			InputSchritt_1(Input);
+			struct StackElement Test = Pop();
+			printf("%s", Test.InputText);
+			Test = Pop();
+			printf("%s", Test.InputText);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}
+	
+	
 
 	
 
-	{ 
-		char input[256];
-		History(0, 0);
-		NeuesElement(fgets(input, sizeof(input), stdin));
-		NeuesElement(fgets(input, sizeof(input), stdin));
-		NeuesElement(fgets(input, sizeof(input), stdin));
-		NeuesElement(fgets(input, sizeof(input), stdin));
-		NeuesElement(fgets(input, sizeof(input), stdin));
-		NeuesElement(fgets(input, sizeof(input), stdin));
-	}
+
+
+
+
 
 
 
